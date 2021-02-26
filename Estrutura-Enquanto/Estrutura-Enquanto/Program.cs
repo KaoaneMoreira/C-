4b59a9a36bd87c59473883826da837691cbe009c
Kaoane Moreira <kaoane.moreira3@gmail.com>
2021-02-26 18:51:10 -0300

    Estrutura sequencial e condicional

---
+++ Estrutura-Enquanto/Estrutura-Enquanto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estrutura_Enquanto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade, soma, media;
+            int cont = 0;
+
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            soma = 0.0;
+
+            while (idade >= 0)
+            {
+                soma = idade + soma;
+                cont = cont + 1;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            media = soma / cont;
+            Console.WriteLine();
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
---
+++ exercicio-estrutura-condicional/exercicio-estrutura-condicional/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_estrutura_condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            String[] vet = Console.ReadLine().Split(' ');
+            int minutos;
+            double conta;
+
+            minutos = int.Parse(Console.ReadLine());
+            conta = 50.0;
+
+            if (minutos > 100)
+            {
+                conta = conta + (minutos - 100) * 2.0;
+            }
+
+            Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
